c4ae5132f8dba0094e7c39ee4a5112a6ce7551cf
swapnilwakchaure <swapnilwakchaure2411999@gmail.com>
2024-07-20 15:50:51 +0530

    realtime-device-tracker-application-done

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+  console.log("connected");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(8080, () => {
+  console.log("server is running on port 8080");
+});
